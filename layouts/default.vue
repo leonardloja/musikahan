3978da310e3a3bab14dccc4261488b2fa2276ca9
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-02-09 21:33:18 +0800

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+
+const route = useRoute();
+</script>
+
+<template>
+    <div class="min-h-screen w-full">
+        <nav
+            class="flex flex-row justify-center items-center gap-4 py-4 h-[10vh] sticky top-0 z-50 backdrop-blur bg-white/70 shadow-md">
+            <NuxtLink href="/">
+                <img class="h-12" src="~/assets/images/logo-16x9.png" alt="Logo">
+            </NuxtLink>
+            <NuxtLink href="/" :class="['un text-lg font-semibold', {
+                'text-[var(--primary)]': route.path == '/'
+            }]">
+                Home
+            </NuxtLink>
+            <NuxtLink href="/schedule" :class="['un text-lg font-semibold', {
+                'text-[var(--primary)]': route.path == '/schedule'
+            }]">
+                Schedule
+            </NuxtLink>
+            <NuxtLink href="/news" :class="['un text-lg font-semibold', {
+                'text-[var(--primary)]': route.path == '/news'
+            }]">
+                News
+            </NuxtLink>
+            <NuxtLink href="/competitive-event-guidelines" :class="['un text-lg font-semibold', {
+                'text-[var(--primary)]': route.path == '/competitive-event-guidelines'
+            }]">
+                Event Guidelines
+            </NuxtLink>
+        </nav>
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
+
+<style scoped>
+.un {
+    display: inline-block;
+    background-image: linear-gradient(var(--primary) 0 0);
+    background-position: 0 100%;
+    /*OR bottom left*/
+    background-size: 0% 2px;
+    background-repeat: no-repeat;
+    padding-bottom: 2px;
+    transition:
+        background-size 0.3s,
+        background-position 0s 0.3s;
+}
+
+.un:hover {
+    background-position: 100% 100%;
+    /*OR bottom right*/
+    background-size: 100% 2px;
+    color: var(--primary);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <div class="min-h-screen">
+        <div
+            class="bg-[url('~/assets/images/background.jpg')] bg-[size:50%] h-[90vh] flex flex-col justify-center text-center">
+            <div class="bg-[#239b51 backdrop-blur py-12">
+                <div
+                    class="pb-2 text-stroke font-black text-6xl bg-gradient-to-t text-transparent inline-block bg-clip-text text-center from-[#ff853c] to-[#ffd440]">
+                    "Truly Tagum - the City in Harmony"
+                </div>
+                <div
+                    class="pb-2 text-stroke font-black text-3xl bg-gradient-to-t text-transparent inline-block bg-clip-text text-center from-[#ff853c] to-[#ffd440]">
+                    "MUSIKAHAN: Pagpalig-on sa Kabaguhan, Kahiusaan ug Kadaugan!"
+                </div>
+                <div class="mt-4 font-bold text-xl text-white text-center">
+                    February 16 to 19, 2023
+                    <div>
+                        City of Tagum (Various Venues)
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="h-screen">
+            latest news
+        </div>
+    </div>
+</template>
